f5a799a81e51b997d2bae4e5dbe7b1b88c6ba252
leddie24 <leddie24@gmail.com>
2016-12-06 22:17:33 -0800

    Update choice field group component page, clean up HBS for choice field group and radio button

---
+++ src/documentation/pages/Components/ChoiceFieldGroup/ChoiceFieldGroup.js
@@ -0,0 +1,14 @@
+var data = {
+   componentName: "ChoiceFieldGroup",
+   overview: "<p>Used to indicate a single choice from multiple options.</p>",
+   variants: [
+      {
+         name: "Default ChoiceFieldGroup",
+         templateFile: "ChoiceFieldGroupElem",
+         model: "ChoiceFieldGroupModel"
+      }
+   ],
+   jsFile: "ChoiceFieldGroupJS"
+}
+
+module.exports = data;
