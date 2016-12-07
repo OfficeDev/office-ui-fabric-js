d5ca452e9bc0a18ccb3847395787e5976b8fd39a
leddie24 <leddie24@gmail.com>
2016-12-07 10:43:12 -0800

    Update pivot component page

---
+++ src/documentation/pages/Components/Pivot/Pivot.js
@@ -0,0 +1,29 @@
+var data = {
+   componentName: "Pivot",
+   overview: "<p>A layout component that allows a user to switch between different sets of content or filters on the content currently visible. This is in contrast to a purely navigational UI element such as NavBar or a commanding element such as CommandBar. An example would be Read vs. Unread items in the Outlook Web App.</p>",
+   variants: [
+      {
+         name: "Default Pivot",
+         templateFile: "PivotExample",
+         model: "PivotDefaultExampleModel"
+      },
+      {
+         name: "Large Pivot",
+         templateFile: "PivotExample",
+         model: "PivotLargeExampleModel"
+      },
+      {
+         name: "Tabs Pivot",
+         templateFile: "PivotExample",
+         model: "PivotTabsExampleModel"
+      },
+      {
+         name: "Large Tabs Pivot",
+         templateFile: "PivotExample",
+         model: "PivotLargeTabsExampleModel"
+      }
+   ],
+   jsFile: "PivotExampleJS"
+}
+
+module.exports = data;
