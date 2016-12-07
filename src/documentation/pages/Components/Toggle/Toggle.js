e2a8a9f092e7d4b8a3a0e8e950c2b9e1d3a56f36
leddie24 <leddie24@gmail.com>
2016-12-06 21:10:49 -0800

    Update Toggle component models and clean up HBS files

---
+++ src/documentation/pages/Components/Toggle/Toggle.js
@@ -0,0 +1,24 @@
+var data = {
+   componentName: "Toggle",
+   overview: "<p>Allows for turning on or off a setting. This is best suited for simple, singular configuration (e.g. application level settings) whereas the ChoiceField is better suited when there is a longer list of individual choices or in a form (e.g. signing up for different newsletters during registration for an event).</p>",
+   variants: [
+      {
+         name: "Default Toggle",
+         templateFile: "ToggleElem",
+         model: "ToggleBasicModel"
+      },
+      {
+         name: "Disabled Toggle",
+         templateFile: "ToggleElem",
+         model: "ToggleDisabledModel"
+      },
+      {
+         name: "Text Left Toggle",
+         templateFile: "ToggleElem",
+         model: "ToggleTextLeftModel"
+      }
+   ],
+   jsFile: "ToggleJS"
+}
+
+module.exports = data;
