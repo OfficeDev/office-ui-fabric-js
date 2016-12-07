6b8ca6e8105e920c1d4ecc6f95e9debe7c92420b
leddie24 <leddie24@gmail.com>
2016-12-06 21:28:18 -0800

    Update TextField component page and models, clean up HBS

---
+++ src/documentation/pages/Components/TextField/TextField.js
@@ -0,0 +1,34 @@
+var data = {
+   componentName: "TextField",
+   overview: "<p>Allows for the input of text. Can be a single line or multiple lines. Typically used to accept user input within a form.</p>",
+   variants: [
+      {
+         name: "Default TextField",
+         templateFile: "TextFieldExample",
+         model: "TextFieldExampleModel"
+      },
+      {
+         name: "Multiline TextField",
+         templateFile: "TextFieldExample",
+         model: "TextFieldMultilineExampleModel"
+      },
+      {
+         name: "Placeholder TextField",
+         templateFile: "TextFieldExample",
+         model: "TextFieldPlaceholderExampleModel"
+      },
+      {
+         name: "Underlined TextField",
+         templateFile: "TextFieldExample",
+         model: "TextFieldUnderlinedExampleModel"
+      },
+      {
+         name: "Disabled TextField",
+         templateFile: "TextFieldExample",
+         model: "TextFieldDisabledExampleModel"
+      }
+   ],
+   jsFile: "TextFieldExampleJS"
+}
+
+module.exports = data;
