46138a5015583c52e5bef9a578390aa4261b4af7
leddie24 <leddie24@gmail.com>
2016-12-07 17:07:50 -0800

    Working on Dialog component page

---
+++ src/documentation/pages/Components/Dialog/Dialog.js
@@ -0,0 +1,14 @@
+var data = {
+  componentName: "Dialog",
+  overview: "<p>Displays content that temporarily blocks interactions with the application. It is primarily used for confirmation tasks, light-weight creation or edit tasks, and simple management tasks.</p>",
+  variants: [
+    {
+      name: "Default Dialog",
+      templateFile: "DialogExample",
+      model: "DialogExampleModel"
+    }
+  ],
+  jsFile: "DialogDocAllElementsJS"
+}
+
+module.exports = data;
--- src/documentation/pages/Components/Dialog/examples/DialogExampleModel.js
+++ src/documentation/pages/Components/Dialog/examples/DialogExampleModel.js
@@ -1,57 +1,59 @@
 var DialogExampleModel = {
-  "default": {
-    "title": "All emails together",
-    "subText": "Your Inbox has changed. No longer does it include favorites, it is a singular destination for your emails.",
-    "components": [
-      {
-        "component": "CheckBox",
-        "props":  {
-          "label": "Option1",
-          "modifier": "",
-          "name": "checkboxa",
-          "id": "checkboxa",
-          "checked": false,
-          "disabled": false,
-          "type": "checkbox"
-        }
-      },
-      {
-        "component": "CheckBox",
-        "props":  {
-          "label": "Option2",
-          "modifier": "",
-          "name": "checkboxa",
-          "id": "checkboxa",
-          "checked": false,
-          "disabled": false,
-          "type": "checkbox"
-        }
+  "title": "All emails together",
+  "subText": "Your Inbox has changed. No longer does it include favorites, it is a singular destination for your emails.",
+  "components": [
+    {
+      "component": "CheckBox",
+      "props":  {
+        "label": "Option1",
+        "modifier": "",
+        "name": "checkboxa",
+        "id": "checkboxa",
+        "checked": false,
+        "disabled": false,
+        "type": "checkbox"
       }
-    ],
-    "actions": [
-      {
-        "component": "Button",
-        "props": {
-          "label": "Save",
-          "tag": "button",
-          "modifier": "primary",
-          "customClasses": "ms-Dialog-action"
-        }
-      },
-      {
-        "component": "Button",
-        "props": {
-          "label": "Cancel",
-          "tag": "button",
-          "customClasses": "ms-Dialog-action"
-        }
+    },
+    {
+      "component": "CheckBox",
+      "props":  {
+        "label": "Option2",
+        "modifier": "",
+        "name": "checkboxa",
+        "id": "checkboxa",
+        "checked": false,
+        "disabled": false,
+        "type": "checkbox"
       }
-    ],
-    "contextualHostProps":  {
-      "state": "is-open"
     }
-  },
-  "blocking": {
+  ],
+  "actions": [
+    {
+      "component": "Button",
+      "props": {
+        "label": "Save",
+        "tag": "button",
+        "modifier": "primary",
+        "customClasses": "ms-Dialog-action"
+      }
+    },
+    {
+      "component": "Button",
+      "props": {
+        "label": "Cancel",
+        "tag": "button",
+        "customClasses": "ms-Dialog-action"
+      }
+    }
+  ],
+  "contextualHostProps":  {
+    "state": "is-open"
+  }
+}
+
+module.exports = DialogExampleModel;
+
+/*"blocking": {
     "modifier": "blocking",
     "title": "Unsaved changes",
     "subText": "Are you sure you want to discard these changes?.",
@@ -226,7 +228,4 @@ var DialogExampleModel = {
     "contextualHostProps":  {
       "state": "is-open"
     }
-  }
-}
-
-module.exports = DialogExampleModel;
+  }*/
