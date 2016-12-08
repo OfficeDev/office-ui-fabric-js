923445b8cc77d8dc458d2a38c3b0270805cf4344
Edward Liu <v-edwliu@microsoft.com>
2016-12-08 13:40:17 -0800

    add datepicker json data

---
+++ src/documentation/pages/Components/DatePicker/DatePicker.js
@@ -0,0 +1,15 @@
+var data = {
+  componentName: "DatePicker",
+  overview: "<p>Allows for the selection of a date into a text field.</p>",
+  variants: [
+    {
+      name: "Default DatePicker",
+      templateFile: "DatePickerExample",
+      model: null
+    }
+  ],
+  jsFile: "DatePickerExampleJS",
+  isDatePicker: true
+}
+
+module.exports = data;
