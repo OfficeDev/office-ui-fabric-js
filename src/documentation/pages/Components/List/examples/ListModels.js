a8639161881600707d724dcb4a403cdabdbe2147
leddie24 <leddie24@gmail.com>
2016-12-23 18:35:23 -0800

    Reduce Facepile and list html so its easier to view.  Fix spinner JS

--- src/documentation/pages/Components/List/examples/ListModels.js
+++ src/documentation/pages/Components/List/examples/ListModels.js
@@ -29,50 +29,6 @@ var ListModels = {
         "secondaryText": "Meeting notes",
         "tertiaryText": "Today we discussed the importance of a, b, and c in regards to d.",
         "metaText": "2:42p",
-        "state": "is-unread is-selectable",
-        "actions": [{}]
-      }
-    },
-    {
-      "name": "ListItem",
-      "props": {
-        "primaryText": "Alton Lafferty",
-        "secondaryText": "Meeting notes",
-        "tertiaryText": "Today we discussed the importance of a, b, and c in regards to d.",
-        "metaText": "2:42p",
-        "state": "is-unread is-selectable",
-        "actions": [{}]
-      }
-    },
-    {
-      "name": "ListItem",
-      "props": {
-        "primaryText": "Alton Lafferty",
-        "secondaryText": "Meeting notes",
-        "tertiaryText": "Today we discussed the importance of a, b, and c in regards to d.",
-        "metaText": "2:42p",
-        "state": "is-unread is-selectable",
-        "actions": [{}]
-      }
-    },
-    {
-      "name": "ListItem",
-      "props": {
-        "primaryText": "Alton Lafferty",
-        "secondaryText": "Meeting notes",
-        "tertiaryText": "Today we discussed the importance of a, b, and c in regards to d.",
-        "metaText": "2:42p",
-        "state": "is-selectable",
-        "actions": [{}]
-      }
-    },
-    {
-      "name": "ListItem",
-      "props": {
-        "primaryText": "Alton Lafferty",
-        "secondaryText": "Meeting notes",
-        "tertiaryText": "Today we discussed the importance of a, b, and c in regards to d.",
-        "metaText": "2:42p",
         "state": "is-selectable",
         "actions": [{}]
       }
