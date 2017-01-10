c8925fa19dd09653ab0523299a35979794357368
leddie24 <leddie24@gmail.com>
2017-01-10 10:12:20 -0800

    #295961 Fabric JS Website Bug: ChoiceFieldGroup could link to Fabric React ChoiceGroup

--- src/documentation/pages/Components/ChoiceFieldGroup/ChoiceFieldGroup.js
+++ src/documentation/pages/Components/ChoiceFieldGroup/ChoiceFieldGroup.js
@@ -1,6 +1,8 @@
 var data = {
    componentName: "ChoiceFieldGroup",
    overview: "<p>Used to indicate a single choice from multiple options.</p>",
+   availableReact: true,
+   reactComponent: 'ChoiceGroup',
    variants: [
       {
          name: "Default ChoiceFieldGroup",
